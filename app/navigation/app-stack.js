4caa88ed1977182e70b6fcc548c5ffaec1d15f8c
MuazzezA <ayd.muazzez@gmail.com>
2023-06-16 01:34:09 +0300

    yapı değiştirildi

--- app/navigation/app-stack.js
+++ app/navigation/app-stack.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import NewsStack from './news-stack';
-import AboutStack from './about-stack';
-import GiftStack from './gift-stack';
-import UnitsStack from './units-stack';
-import {HomeScreen, SplashScreen} from '../screens';
+
+import {SplashScreen} from '../screens';
+import TabNav from './tab-navigator';
 const Stack = createNativeStackNavigator();
 
 const AppStack = () => {
@@ -16,11 +14,7 @@ const AppStack = () => {
           headerShown: false,
         }}>
         <Stack.Screen name="splash-screen" component={SplashScreen} />
-        <Stack.Screen name="home-screen" component={HomeScreen} />
-        <Stack.Screen name="news-stack" component={NewsStack} />
-        <Stack.Screen name="about-stack" component={AboutStack} />
-        <Stack.Screen name="gift-stack" component={GiftStack} />
-        <Stack.Screen name="units-stack" component={UnitsStack} />
+        <Stack.Screen name="tab-navigator" component={TabNav} />
       </Stack.Navigator>
     </NavigationContainer>
   );
