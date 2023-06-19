1c499aca1473e49ca46e47025c5aa30662e77b63
MuazzezA <ayd.muazzez@gmail.com>
2023-06-19 22:50:23 +0300

    edit splash screen

--- app/screens/Splash/index.js
+++ app/screens/Splash/index.js
@@ -1,53 +1,24 @@
 import React, {useEffect} from 'react';
-import {View, Text, Animated, Image} from 'react-native';
+import {View, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import Paw from '../../../assets/svg/Paw.svg';
 import styles from './styles';
-import {appConfig} from '../../utils/appConfig';
 
 export const SplashScreen = () => {
   const navigation = useNavigation();
-  const opacity = new Animated.Value(0.4);
-
-  const fadeInOut = Animated.sequence([
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 600,
-      useNativeDriver: true,
-    }),
-    Animated.timing(opacity, {
-      toValue: 0.4,
-      duration: 600,
-      useNativeDriver: true,
-    }),
-  ]);
 
   useEffect(() => {
-    Animated.loop(fadeInOut).start();
     setTimeout(() => {
       navigation.navigate('tab-navigator');
-      fadeInOut.stop();
     }, 3000);
   }, []);
 
   return (
     <View style={styles.container}>
-      <View style={styles.appTextContainer}>
-        <Text style={styles.appText}>{appConfig.appName}</Text>
-      </View>
       <Image
-        style={styles.catImage}
-        source={require('../../../assets/images/Cat.png')}
+        resizeMode="cover"
+        style={styles.image}
+        source={require('../../../assets/images/Splash.jpeg')}
       />
-      <Animated.View
-        style={[
-          {
-            opacity: opacity,
-          },
-          styles.pawContainer,
-        ]}>
-        <Paw height="100" width="100" />
-      </Animated.View>
     </View>
   );
 };
